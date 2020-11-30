5d4cbb89ad8036a26e92201b497feb3d95c0379a
Praneeth021 <kadampallypraneeth987@gmail.com>
2020-11-30 13:28:32 +0530

    changes to login page

--- app.py
+++ app.py
@@ -83,5 +83,11 @@ def create_database():
 def Main():
     return render_template('index.html')
 
+
+@app.route('/adminlogin')
+
+def AdminLogin():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
